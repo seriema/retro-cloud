8d2e9e27f0fc0c6db479111ce9c59aa7f3362612
John-Philip Johansson <seriema@gmail.com>
2019-12-28 11:17:32 +0000

    Make install-az-module idempotent

--- raspberry-pi/install-az-module.ps1
+++ raspberry-pi/install-az-module.ps1
@@ -1,9 +1,11 @@
 ###################################
 # Install the Azure PowerShell module 
 
-# Install for the active user:
+# Install for the active user
 Install-Module -Name Az -AllowClobber -Scope CurrentUser
 # need to trust source: "PSGallery"
 
-# Connect to Azure with a browser sign in token
-Connect-AzAccount
+# Connect to Azure with a browser sign in token, if not already logged in
+if ((Get-AzContext) -eq $null) {
+    Connect-AzAccount
+}
