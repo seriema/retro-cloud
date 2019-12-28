f8f9c72c95e42337d8e24e956bb2c38be9bf22e7
John-Philip Johansson <seriema@gmail.com>
2019-12-28 09:43:36 +0000

    Clean up uninstall-ps.sh script

--- raspberry-pi/uninstall-ps.sh
+++ raspberry-pi/uninstall-ps.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
+# https://docs.microsoft.com/en-us/powershell/scripting/install/installing-powershell-core-on-linux?view=powershell-6#raspbian
+
+###################################
+# Delete the extracted PowerShell
 
 rm -rf ~/powershell
 
+# Remove unused apt packages
 sudo apt autoremove
