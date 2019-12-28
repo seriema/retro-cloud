85398bfa72ae85a6242dc12a74d2a20f163ebfca
John-Philip Johansson <seriema@gmail.com>
2019-12-28 16:40:16 +0000

    Noninteractive Az module install

--- raspberry-pi/install-az-module.ps1
+++ raspberry-pi/install-az-module.ps1
@@ -1,11 +1,12 @@
 ###################################
-# Install the Azure PowerShell module 
+# Install the Azure PowerShell module
 
-# Install for the active user
-Install-Module -Name Az -AllowClobber -Scope CurrentUser
-# need to trust source: "PSGallery"
+# Install for the active user, if not already installed.
+# Note: The -Force parameter is needed to avoid a user prompt, but also does a reinstall if already installed.
+Install-Module -Name Az -AllowClobber -Scope CurrentUser -Force
 
-# Connect to Azure with a browser sign in token, if not already logged in
+# Connect to Azure with a browser sign in token, if not already logged in.
+# Note: user prompt!
 if ((Get-AzContext) -eq $null) {
     Connect-AzAccount
 }
