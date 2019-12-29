56db2f195dcb0e03aa81ce0b1d35f4c221561872
John-Philip Johansson <seriema@gmail.com>
2019-12-29 23:15:04 +0000

    Use environment variables for settings in mount-share.sh
    It's not a safe approach but convenient for development and re-using the
    variables accross scripts.
    
    Also fixed an issue with the .emulationstation path that would create
    the folder in the wrong place.

--- virtual-machine/mount-share.sh
+++ virtual-machine/mount-share.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 # https://docs.microsoft.com/en-us/azure/storage/files/storage-how-to-use-files-linux#mounting-azure-file-share
 
+# Abort on error
+set -e
+
 echo 'Install Prerequisites'
 
 sudo apt-get update
@@ -10,17 +13,7 @@ sudo apt-get install cifs-utils
 echo 'Create a persistent mount point for the Azure file share with /etc/fstab'
 
 echo 'Create a folder for the mount point'
-
-##TODO the variables here should come from the rpi, but how? can I use environment variables?
-resourceGroupName='secret'
-storageAccountName='secret'
-storageAccountKey='secret'
-fileShareName='secret'
-smbPath='//secret.file.core.windows.net/skyscraper-cache'
-##
-
 mntPath="/mnt/$storageAccountName/$fileShareName"
-
 sudo mkdir -p $mntPath
 
 echo 'Create a credential file to store the username and password for the file share.'
@@ -42,11 +35,13 @@ sudo chmod 600 $smbCredentialFile
 
 echo 'Append the mount to /etc/fstab'
 if [ -z "$(grep $smbPath\ $mntPath /etc/fstab)" ]; then
-    echo "# The changes below were made by retro-cloud" | sudo tee -a /etc/fstab > /dev/null
+    echo "# RETRO-CLOUD: The changes below were made by retro-cloud" | sudo tee -a /etc/fstab > /dev/null
     # To the right is the original, and the below is what I made some time ago: echo "$smbPath $mntPath cifs nofail,vers=3.0,credentials=$smbCredentialFile,serverino" | sudo tee -a /etc/fstab > /dev/null
     echo "$smbPath $mntPath cifs _netdev,nofail,vers=3.0,credentials=$smbCredentialFile,dir_mode=0777,file_mode=0777,serverino" | sudo tee -a /etc/fstab > /dev/null
 else
     echo "/etc/fstab was not modified to avoid conflicting entries as this Azure file share was already present. You may want to double check /etc/fstab to ensure the configuration is as desired."
+    echo "Aborting to avoid issues"
+    exit 0
 fi
 
 echo 'Mount right away'
@@ -56,16 +51,17 @@ sudo mount -a
 echo 'Create Skyscraper output folders to be mounted.'
 gamelists="$mntPath/output/gamelists"
 downloadedMedia="$mntPath/output/downloaded_media"
+emulationstation="$HOME/.emulationstation"
 
 sudo mkdir -p "$gamelists"
 sudo mkdir -p "$downloadedMedia"
-mkdir .emulationstation
+mkdir "$emulationstation"
 
 echo 'Symlink the folders to look like emulationstation' # but this doesn't work?!
-ln -s "$gamelists" .emulationstation
-ln -s "$downloadedMedia" .emulationstation
+ln -s "$gamelists" "$emulationstation"
+ln -s "$downloadedMedia" "$emulationstation"
 
 
 echo 'Done!'
 echo "File share mounted on $mntPath"
-echo "Symlinked folders to ~/.emulationstation"
+echo "Symlinked folders to $emulationstation"
