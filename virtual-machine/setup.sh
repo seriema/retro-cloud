8651abcb04abba14891042a6b3ed6041eaf87f6f
John-Philip Johansson <seriema@gmail.com>
2020-01-04 18:19:38 +0000

    vm: Fix download of Skyscraper config
    It wasn't being put in the expected local directory: .skyscraper
    So the script would fail when trying to copy it over.

--- virtual-machine/setup.sh
+++ virtual-machine/setup.sh
@@ -9,11 +9,13 @@ echo "SETUP: Download scripts to ~/tmp/retro-cloud"
 mkdir -p "$HOME/tmp/retro-cloud"
 cd "$HOME/tmp/retro-cloud"
 branch=vm
-wget -q "https://raw.githubusercontent.com/seriema/retro-cloud/$branch/virtual-machine/.skyscraper/config.ini"
 wget -q "https://raw.githubusercontent.com/seriema/retro-cloud/$branch/virtual-machine/install-skyscraper.sh"
 wget -q "https://raw.githubusercontent.com/seriema/retro-cloud/$branch/virtual-machine/create-vm-share.sh"
 wget -q "https://raw.githubusercontent.com/seriema/retro-cloud/$branch/virtual-machine/mount-az-share.sh"
 wget -q "https://raw.githubusercontent.com/seriema/retro-cloud/$branch/virtual-machine/run-skyscraper.sh"
+# TODO: This should be installed from the install-skyscraper script instead but the branch name in the URL needs to stay in sync here.
+mkdir .skyscraper
+wget -q -O ".skyscraper/config.ini" "https://raw.githubusercontent.com/seriema/retro-cloud/$branch/virtual-machine/.skyscraper/config.ini"
 
 echo "SETUP: Mount Azure File Share"
 bash mount-az-share.sh
