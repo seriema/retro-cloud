70778cc6d7893ba1e5c1c19878dd0b7a7d002009
John-Philip Johansson <seriema@gmail.com>
2020-01-04 14:15:46 +0000

    vm: Improve "share" script names
    It wasn't obvious that the create and mount were creating and mounting
    different things.

--- virtual-machine/setup.sh
+++ virtual-machine/setup.sh
@@ -6,18 +6,18 @@ cd "$HOME/tmp/retro-cloud"
 branch=vm
 wget -q "https://raw.githubusercontent.com/seriema/retro-cloud/$branch/virtual-machine/.skyscraper/config.ini"
 wget -q "https://raw.githubusercontent.com/seriema/retro-cloud/$branch/virtual-machine/install-skyscraper.sh"
-wget -q "https://raw.githubusercontent.com/seriema/retro-cloud/$branch/virtual-machine/create-share.sh"
-wget -q "https://raw.githubusercontent.com/seriema/retro-cloud/$branch/virtual-machine/mount-share.sh"
+wget -q "https://raw.githubusercontent.com/seriema/retro-cloud/$branch/virtual-machine/create-vm-share.sh"
+wget -q "https://raw.githubusercontent.com/seriema/retro-cloud/$branch/virtual-machine/mount-az-share.sh"
 wget -q "https://raw.githubusercontent.com/seriema/retro-cloud/$branch/virtual-machine/run-skyscraper.sh"
 
 echo "SETUP: Mount Azure File Share"
-bash mount-share.sh
+bash mount-az-share.sh
 
 echo "SETUP: Install Skyscraper"
 bash install-skyscraper.sh
 
 echo "SETUP: Create share for Raspberry Pi"
-bash create-share.sh
+bash create-vm-share.sh
 
 echo "SETUP: Delete ~/tmp/retro-cloud"
 cd
