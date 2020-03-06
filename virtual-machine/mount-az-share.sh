d13bc09fd2535802a86b72e33d2eaa9405abb0ff
John-Philip Johansson <seriema@gmail.com>
2020-03-06 16:20:56 +0100

    vm: Use naming convention for env vars
    Removed the unused resource group name.
    
    Learned this from shellcheck, rule SC2154 "var is referenced but not assigned"
    because it only triggers for variables with lowercase characters in their
    name due to the standard convention of using lowercase variable names for
    unexported, local variables.
    https://github.com/koalaman/shellcheck/wiki/SC2154

--- virtual-machine/mount-az-share.sh
+++ virtual-machine/mount-az-share.sh
@@ -20,22 +20,22 @@ if [ ! -d "/etc/smbcredentials" ]; then
     sudo mkdir "/etc/smbcredentials"
 fi
 
-smbCredentialFile="/etc/smbcredentials/$storageAccountName.cred"
+smbCredentialFile="/etc/smbcredentials/$RETROCLOUD_AZ_STORAGE_ACCOUNT_NAME.cred"
 if [ ! -f $smbCredentialFile ]; then
-    echo "username=$storageAccountName" | sudo tee $smbCredentialFile > /dev/null
-    echo "password=$storageAccountKey" | sudo tee -a $smbCredentialFile > /dev/null
+    echo "username=$RETROCLOUD_AZ_STORAGE_ACCOUNT_NAME" | sudo tee $smbCredentialFile > /dev/null
+    echo "password=$RETROCLOUD_AZ_STORAGE_ACCOUNT_KEY" | sudo tee -a $smbCredentialFile > /dev/null
     echo "Credential file $smbCredentialFile created"
 
     echo 'Change permissions on the credential file so only root can read or modify the password file.'
     sudo chmod 600 $smbCredentialFile
-else 
+else
     echo "The credential file $smbCredentialFile already exists, and was not modified."
 fi
 
 echo 'Create a persistent mount point for the Azure file share with /etc/fstab'
-if [ -z "$(grep $smbPath\ $mntPath /etc/fstab)" ]; then
+if [ -z "$(grep $RETROCLOUD_AZ_FILE_SHARE_URL\ $mntPath /etc/fstab)" ]; then
     echo "# RETRO-CLOUD: The changes below were made by retro-cloud" | sudo tee -a /etc/fstab > /dev/null
-    echo "$smbPath $mntPath cifs _netdev,nofail,vers=3.0,credentials=$smbCredentialFile,dir_mode=0777,file_mode=0777,serverino" | sudo tee -a /etc/fstab > /dev/null
+    echo "$RETROCLOUD_AZ_FILE_SHARE_URL $mntPath cifs _netdev,nofail,vers=3.0,credentials=$smbCredentialFile,dir_mode=0777,file_mode=0777,serverino" | sudo tee -a /etc/fstab > /dev/null
 else
     echo "/etc/fstab was not modified to avoid conflicting entries as this Azure file share was already present. You may want to double check /etc/fstab to ensure the configuration is as desired."
     echo "Aborting to avoid issues"
@@ -44,7 +44,7 @@ fi
 
 # Debugging: Mount the drive without persisting it
 # https://docs.microsoft.com/en-us/azure/storage/files/storage-how-to-use-files-linux#mount-the-azure-file-share-on-demand-with-mount
-# sudo mount -t cifs $smbPath $mntPath -o _netdev,nofail,vers=3.0,username=$storageAccountName,password=$storageAccountKey,dir_mode=0777,file_mode=0777,serverino
+# sudo mount -t cifs $RETROCLOUD_AZ_FILE_SHARE_URL $mntPath -o _netdev,nofail,vers=3.0,username=$RETROCLOUD_AZ_STORAGE_ACCOUNT_NAME,password=$RETROCLOUD_AZ_STORAGE_ACCOUNT_KEY,dir_mode=0777,file_mode=0777,serverino
 
 echo 'Mount now to avoid a reboot'
 sudo mount -a
