954788e0edb0fdbd851686069f87200ebbb5a43e
John-Philip Johansson <john-philip.johansson@episerver.com>
2020-03-07 22:31:51 +0100

    vm: Make download and copy scripts chatty
    Silent install is nice and should be an option in the future, but during
    development it's just time consuming to not see what went wrong.

--- virtual-machine/install-skyscraper.sh
+++ virtual-machine/install-skyscraper.sh
@@ -14,13 +14,13 @@ sudo apt-get install build-essential qt5-default -y > /dev/null
 echo 'Install Skyscraper (takes a while)'
 mkdir -p "$HOME/skysource"
 cd "$HOME/skysource"
-wget -q -O - https://raw.githubusercontent.com/muldjord/skyscraper/master/update_skyscraper.sh | bash > /dev/null
+wget -nv -O - https://raw.githubusercontent.com/muldjord/skyscraper/master/update_skyscraper.sh | bash > /dev/null
 cd -
 
 echo 'Configure Skyscraper'
 # ~/.skyscraper is created when Skyscraper runs the first time but we need it now.
 mkdir -p "$HOME/.skyscraper"
-cp .skyscraper/config.ini "$HOME/.skyscraper/config.ini"
+cp -v .skyscraper/config.ini "$HOME/.skyscraper/config.ini"
 
 # If these variables aren't available, make sure this script is running in interactive mode (https://stackoverflow.com/a/43660876) and mount-az-share.sh.
 sed -i -e "s+RETROCLOUD_INPUTFOLDER+$RETROCLOUD_ROMS+g" "$HOME/.skyscraper/config.ini"
@@ -29,7 +29,7 @@ sed -i -e "s+RETROCLOUD_MEDIAFOLDER+$RETROCLOUD_SKYSCRAPER_MEDIAFOLDER+g" "$HOME
 sed -i -e "s+RETROCLOUD_CACHEFOLDER+$RETROCLOUD_SKYSCRAPER_CACHEFOLDER+g" "$HOME/.skyscraper/config.ini"
 
 echo 'Copy run script to user root'
-cp local/run-skyscraper.sh "$HOME/run-skyscraper.sh"
+cp -v local/run-skyscraper.sh "$HOME/run-skyscraper.sh"
 # Make it executable
 chmod 777 "$HOME/run-skyscraper.sh"
 
--- virtual-machine/setup.sh
+++ virtual-machine/setup.sh
@@ -10,14 +10,14 @@ set -u
 echo "SETUP: Download scripts to ~/tmp/retro-cloud"
 mkdir -p "$HOME/tmp/retro-cloud"
 cd "$HOME/tmp/retro-cloud"
-wget -q "https://raw.githubusercontent.com/seriema/retro-cloud/$branch/virtual-machine/install-skyscraper.sh"
-wget -q "https://raw.githubusercontent.com/seriema/retro-cloud/$branch/virtual-machine/create-vm-share.sh"
-wget -q "https://raw.githubusercontent.com/seriema/retro-cloud/$branch/virtual-machine/mount-az-share.sh"
+wget -nv "https://raw.githubusercontent.com/seriema/retro-cloud/$branch/virtual-machine/install-skyscraper.sh"
+wget -nv "https://raw.githubusercontent.com/seriema/retro-cloud/$branch/virtual-machine/create-vm-share.sh"
+wget -nv "https://raw.githubusercontent.com/seriema/retro-cloud/$branch/virtual-machine/mount-az-share.sh"
 # TODO: This should be installed from the install-skyscraper script instead but the branch name in the URL needs to stay in sync here.
 mkdir .skyscraper
-wget -q -O ".skyscraper/config.ini" "https://raw.githubusercontent.com/seriema/retro-cloud/$branch/virtual-machine/.skyscraper/config.ini"
+wget -nv -O ".skyscraper/config.ini" "https://raw.githubusercontent.com/seriema/retro-cloud/$branch/virtual-machine/.skyscraper/config.ini"
 mkdir "local"
-wget -q -O "local/run-skyscraper.sh" "https://raw.githubusercontent.com/seriema/retro-cloud/$branch/virtual-machine/local/run-skyscraper.sh"
+wget -nv -O "local/run-skyscraper.sh" "https://raw.githubusercontent.com/seriema/retro-cloud/$branch/virtual-machine/local/run-skyscraper.sh"
 
 echo "SETUP: Mount Azure File Share"
 bash mount-az-share.sh
