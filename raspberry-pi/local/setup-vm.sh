bc3d5428caa5b2e747c8d6da9bdfa63ce7460ace
John-Philip Johansson <john-philip.johansson@episerver.com>
2020-03-07 22:31:27 +0100

    rpi: Make download and copy scripts chatty
    Silent run is nice and should be an option in the future, but during
    development it's just time consuming to not see what went wrong.

--- raspberry-pi/local/setup-vm.sh
+++ raspberry-pi/local/setup-vm.sh
@@ -5,6 +5,6 @@ branch=${1:-develop}
 # Abort on error, and error if variable is unset
 set -eu
 
-ssh $RETROCLOUD_VM_USER@$RETROCLOUD_VM_IP "wget -q https://raw.githubusercontent.com/seriema/retro-cloud/$branch/virtual-machine/setup.sh"
+ssh $RETROCLOUD_VM_USER@$RETROCLOUD_VM_IP "wget -nv https://raw.githubusercontent.com/seriema/retro-cloud/$branch/virtual-machine/setup.sh"
 ssh $RETROCLOUD_VM_USER@$RETROCLOUD_VM_IP "bash -i setup.sh $branch"
 ssh $RETROCLOUD_VM_USER@$RETROCLOUD_VM_IP "rm setup.sh"
