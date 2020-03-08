c61b95076c515237d97b33abb7f195d5ba336994
John-Philip Johansson <seriema@gmail.com>
2020-03-08 14:05:24 +0100

    rpi: Make ssh-vm script more generic
    It now takes an optional parameter so it can be used by other scripts to
    run commands on the VM.

--- raspberry-pi/local/run-scraper.sh
+++ raspberry-pi/local/run-scraper.sh
@@ -5,6 +5,4 @@ set -e
 # Error if variable is unset
 set -u
 
-# There's a "permission denied" response when sending "./run-skyscraper.sh"
-ssh $RETROCLOUD_VM_USER@$RETROCLOUD_VM_IP "./run-skyscraper.sh"
-# ssh $RETROCLOUD_VM_USER@$RETROCLOUD_VM_IP "bash -i run-skyscraper.sh"
+bash -i ssh-vm.sh "./run-skyscraper.sh"
