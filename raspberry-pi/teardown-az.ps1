4017ba2300f9875d9e12f1fc0e0c077f00984968
John-Philip Johansson <seriema@gmail.com>
2020-03-15 14:06:26 +0000

    rpi: Teardown without user waiting
    Waiting 15-20 minutes for resource teardown without any output (despite
    Verbose and Debug) is indistinguishable from a hanged process. The
    `-AsJob` removes the prompt, even when combined with `-Confirm`, so a
    custom prompt is added.

--- raspberry-pi/teardown-az.ps1
+++ raspberry-pi/teardown-az.ps1
@@ -9,9 +9,17 @@ $DebugPreference = "Continue"
 
 'Delete resources from Azure ...'
 if (!$env:AZURE_SERVICE_PRINCIPAL_SECRET) {
-    '... are you sure?'
-    # Note: user prompt!
-    Remove-AzResourceGroup -Name $env:RETROCLOUD_AZ_RESOURCE_GROUP
+    '... Note: This operation takes a very long time (15-20 min) so it will only send a command to Azure and not wait for the removal to be complete.'
+
+    $title    = 'WARNING! You will lose all your ROMs and metadata!'
+    $question = 'Are you sure you want to proceed?'
+    $choices  = '&Yes', '&No'
+    $decision = $Host.UI.PromptForChoice($title, $question, $choices, 1)
+    if ($decision -eq 0) {
+        Remove-AzResourceGroup -Name $env:RETROCLOUD_AZ_RESOURCE_GROUP -AsJob
+    } else {
+        Write-Host 'Cancelled.'
+    }
 } else {
     '... assuming automation with Service Principle. Do not wait for resources to be deleted.'
     Remove-AzResourceGroup -Name $env:RETROCLOUD_AZ_RESOURCE_GROUP -Force -AsJob
