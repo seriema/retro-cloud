12319ff41353dc7a74a2eeeedb397f7a9c73c112
John-Philip Johansson <seriema@gmail.com>
2020-03-17 09:33:38 +0000

    docker: Add lifecycle scripts
    Convenient when testing.

---
+++ docker-run.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Abort on error, and error if variable is unset
+set -eu
+
+case "$(uname -m)" in
+    # Assume Windows running Linux containers
+    x86_64) tag="seriema/retro-cloud:amd64" ;;
+    # Assume a Raspberry Pi 3
+    armv7l) tag="seriema/retro-cloud:arm32v7" ;;
+    # Fail
+    *) echo "Unknown architecture: $arch" &% exit 1 ;;
+esac
+
+docker pull "$tag"
+docker run --privileged -it --rm "$tag"
