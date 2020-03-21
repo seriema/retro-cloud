9d820e018b8a57a94067d6427d4db628c4eec24c
John-Philip Johansson <seriema@gmail.com>
2020-03-21 11:20:37 +0100

    vm: Replace wget with curl :birthday:
    It's been on my TODO for a while but curl turned 22 years yesterday so
    that's a good as a reason as any to do it now.
    
    Happy birthday curl! :beers:

--- virtual-machine/dev/test-copy-rom.sh
+++ virtual-machine/dev/test-copy-rom.sh
@@ -4,6 +4,8 @@
 set -eu
 
 # Copy a tiny (58K) freeware game from a verified list on ScummVM
-sudo wget -P "$RETROCLOUD_VM_SHARE/RetroPie/roms/scummvm/" -nv https://www.scummvm.org/frs/extras/Mystery%20House/MYSTHOUS.zip
-# Give it a better name otherwise the scrapers don't find it
-mv "$RETROCLOUD_VM_SHARE/RetroPie/roms/scummvm/MYSTHOUS.zip" "$RETROCLOUD_VM_SHARE/RetroPie/roms/scummvm/MysteryHouse.zip"
+curl -OL https://www.scummvm.org/frs/extras/Mystery%20House/MYSTHOUS.zip
+
+# Move it and give it a better name otherwise the scrapers won't find it
+sudo mkdir -p "$RETROCLOUD_VM_SHARE/RetroPie/roms/scummvm"
+sudo mv "MYSTHOUS.zip" "$_/MysteryHouse.zip"
