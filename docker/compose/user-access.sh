984c2642e778405ee4c819fd49243bfb09f8a669
John-Philip Johansson <seriema@gmail.com>
2020-03-24 11:41:02 +0100

    test(docker): Add disabled runtime test of /dev/fuse
    Because the device /dev/fuse doesn't exist when building the Docker
    image this test is actually testing runtime access, so it would test how
    docker is running and not the image itself. I want to investigate further
    at a later date so I'm adding this test and the information here.
    
    When running the mount-vm-share script it can error when attempting to mount:
    `fuse: failed to open /dev/fuse: Permission denied`
    
    This is due to no write access on /dev/fuse for the user. It only
    happens when running an amd64 image on Windows (I haven't tried it on
    amd64 Linux, but it works on amd32v7 Linux and Raspbian).
    
    Comparison of access rights across platforms:
    ```
    amd64 Windows:    crw-rw----  1 root root  10, 229 Mar 22 14:32 fuse
    arm32v7 Linux:    crw-rw-rw-  1 root root  10, 229 Mar 22 05:58 fuse
    arm32v7 Raspbian: crw-rw-rw-  1 root root  10, 229 Mar 17 18:52 fuse
    ```

--- docker/compose/user-access.sh
+++ docker/compose/user-access.sh
@@ -7,6 +7,7 @@ echo 'Verify access rights on user home and RetroPie'
 [[ $(stat -c %a /home/pi) -eq 755 ]]
 [[ $(stat -c %a /home/pi/RetroPie) -eq 755 ]]
 [[ $(stat -c %a /home/pi/RetroPie-Setup) -eq 755 ]]
+# [[ $(stat -c %a /dev/fuse) -eq 666 ]]
 
 echo 'Verify group memberships'
 [[ $(groups | grep pi) ]]
