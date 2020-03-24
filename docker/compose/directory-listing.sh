22ad1425afab2bb964619a9f7b9cfb555e8e77b9
John-Philip Johansson <seriema@gmail.com>
2020-03-24 16:51:07 +0100

    test(docker): Use double bracket tests
    It's built into Bash and is far safer than [. It's recommended by:
    * RetroPie: https://github.com/RetroPie/RetroPie-Setup/wiki/Shell-Style-Guide#test-and-square-braces
    * StackOverflow: https://stackoverflow.com/a/3427931/703921
    ** Links to: http://mywiki.wooledge.org/BashGuide/Practices#Bash_Tests

--- docker/compose/directory-listing.sh
+++ docker/compose/directory-listing.sh
@@ -5,13 +5,13 @@ set -eux
 
 echo 'Verify number of roms folders. There is one for each successful emultator installed.'
 # Two emulators aren't available on amd64: mame-mame4all, amiga
-if [ "$(uname -m)" = 'armv7l' ]; then
-    emulators='32';
+if [[ $(uname -m) = 'armv7l' ]]; then
+    emulators=32;
 else
-    emulators='30';
+    emulators=30;
 fi
 
-if [ ! $(ls ~/RetroPie/roms | wc -l) == $emulators ]; then
+if  [[ $(ls ~/RetroPie/roms | wc -l) -ne $emulators ]]; then
     echo "Not enough roms folders. Did an emulator fail to install?"
     echo "These were installed:"
     ls ~/RetroPie/roms
@@ -19,7 +19,7 @@ if [ ! $(ls ~/RetroPie/roms | wc -l) == $emulators ]; then
 fi
 
 echo 'Verify that there are no builds left. There is one for each failed build.'
-[ -z "`find /home/pi/RetroPie-Setup/tmp/build -maxdepth 3 -type f`" ]
+[[ -z $(find /home/pi/RetroPie-Setup/tmp/build -maxdepth 3 -type f) ]]
 
 echo 'Verify that line endings of all files are LF. Windows uses CRLF which can get copied over by Docker ADD/COPY.'
-[ ! "$(grep -r $'\r' * -l)" ]
+[[ -z $(grep -r $'\r' * -l) ]]
--- docker/compose/user-access.sh
+++ docker/compose/user-access.sh
@@ -4,13 +4,13 @@
 set -eux
 
 echo 'Verify access rights on user home and RetroPie'
-[ $(stat -c %a /home/pi) -eq 755 ]
-[ $(stat -c %a /home/pi/RetroPie) -eq 755 ]
-[ $(stat -c %a /home/pi/RetroPie-Setup) -eq 755 ]
+[[ $(stat -c %a /home/pi) -eq 755 ]]
+[[ $(stat -c %a /home/pi/RetroPie) -eq 755 ]]
+[[ $(stat -c %a /home/pi/RetroPie-Setup) -eq 755 ]]
 
 echo 'Verify group memberships'
-[ "$(groups | grep pi)" ]
-[ "$(groups | grep sudo)" ]
+[[ $(groups | grep pi) ]]
+[[ $(groups | grep sudo) ]]
 
 echo 'Verify username'
-[ "$(whoami)" = "pi" ]
+[[ $(whoami) = "pi" ]]
