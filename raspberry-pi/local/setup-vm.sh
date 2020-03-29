fa38e8af98673f08c856bb73cd36682deb6248f9
John-Philip Johansson <seriema@gmail.com>
2020-03-29 20:54:20 +0200

    Revert "Revert "docs: Use master branch for release version""
    This reverts commit 82d852d6b9d44ba4e19672a266214c1561fd39cd.

--- raspberry-pi/local/setup-vm.sh
+++ raspberry-pi/local/setup-vm.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Script takes optional parameter for branch name or commit hash.
-branch=${1:-develop}
+branch=${1:-master}
 
 # Abort on error, and error if variable is unset
 set -eu
