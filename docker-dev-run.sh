a784c441703b857dcbe9e24fda8d913879e21f1e
John-Philip Johansson <seriema@gmail.com>
2020-04-02 09:03:57 +0100

    dev: Run Docker with EmulationStation support on ARM
    Use the benefits of a ARM compiled Docker image:
    Run EmulationStation, with controller, graphics, audio, etc, for ultimate
    testing.

--- docker-dev-run.sh
+++ docker-dev-run.sh
@@ -8,13 +8,41 @@ set -eu
 branch=${1:-"$(getBranch)"}
 tag="rc:$branch"
 
-docker run \
-    --cap-add SYS_ADMIN \
-    --device /dev/fuse \
-    --env-file .env \
-    --interactive \
-    --rm \
-    --tty \
-    --volume "$PWD":/home/pi/retro-cloud-source \
-    --workdir /home/pi/retro-cloud-source \
-    "$tag"
+if [[ $(getArch) == "arm32v7" ]]; then
+    docker run \
+        --env PULSE_SERVER=unix:/run/user/1000/pulse/native \
+        --env-file .env \
+        --group-add input \
+        --group-add video \
+        --interactive \
+        --privileged \
+        --rm \
+        --tty \
+        `# Mount various volumes that enable devices (e.g. joypad, video, etc) to pass through to the container.` \
+        --volume /dev/fb0:/dev/fb0 \
+        --volume /dev/input:/dev/input \
+        --volume /dev/shm:/dev/shm \
+        --volume /dev/snd:/dev/snd \
+        --volume /dev/usb:/dev/usb \
+        --volume /dev/vchiq:/dev/vchiq \
+        --volume /dev/vcio:/dev/vcio \
+        --volume /dev/vcsm:/dev/vcsm \
+        --volume /home/pi/.ssh:/home/pi/.ssh:ro \
+        --volume /opt/retropie/configs/all/retroarch:/home/pi/.config/retroarch:ro \
+        --volume /opt/retropie/configs/all/retroarch-joypads:/opt/retropie/configs/all/retroarch-joypads:ro \
+        --volume /opt/vc:/opt/vc \
+        --volume /run/user/1000:/run/user/1000 \
+        --volume /var/run/dbus/:/var/run/dbus/ \
+        "$tag"
+else
+    docker run \
+        --cap-add SYS_ADMIN \
+        --device /dev/fuse \
+        --env-file .env \
+        --interactive \
+        --rm \
+        --tty \
+        --volume "$PWD":/home/pi/retro-cloud-source \
+        --workdir /home/pi/retro-cloud-source \
+        "$tag"
+fi
--- docker-run.sh
+++ docker-run.sh
@@ -6,4 +6,43 @@ set -eu
 . ./helpers.sh
 tag="seriema/retro-cloud:$(getArch)"
 
-docker run --cap-add SYS_ADMIN --device /dev/fuse -it --rm "$tag"
+if [[ $(getArch) == "arm32v7" ]]; then # Raspberry Pi
+    containerInstance=$(docker container create \
+        --env PULSE_SERVER=unix:/run/user/1000/pulse/native \
+        --group-add input \
+        --group-add video \
+        --interactive \
+        --privileged \
+        --rm \
+        --tty \
+        `# Mount various volumes that enable devices (e.g. joypad, video, etc) to pass through to the container.` \
+        --volume /dev/fb0:/dev/fb0 \
+        --volume /dev/input:/dev/input \
+        --volume /dev/shm:/dev/shm \
+        --volume /dev/snd:/dev/snd \
+        --volume /dev/usb:/dev/usb \
+        --volume /dev/vchiq:/dev/vchiq \
+        --volume /dev/vcio:/dev/vcio \
+        --volume /dev/vcsm:/dev/vcsm \
+        --volume /home/pi/.ssh:/home/pi/.ssh:ro \
+        --volume /opt/retropie/configs/all/retroarch:/home/pi/.config/retroarch:ro \
+        --volume /opt/retropie/configs/all/retroarch-joypads:/opt/retropie/configs/all/retroarch-joypads:ro \
+        --volume /opt/vc:/opt/vc \
+        --volume /run/user/1000:/run/user/1000 \
+        --volume /var/run/dbus/:/var/run/dbus/ \
+        "$tag" \
+    )
+
+else # Windows
+    containerInstance=$(docker container create \
+        --cap-add SYS_ADMIN \
+        --device /dev/fuse \
+        --interactive \
+        --rm \
+        --tty \
+        "$tag" \
+    )
+fi
+
+# Run the prepared container
+docker container start --attach --interactive "$containerInstance"
