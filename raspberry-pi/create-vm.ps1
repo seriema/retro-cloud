ab925fbc84825aeb9d85ce43c498190b25d5f54c
John-Philip Johansson <seriema@gmail.com>
2020-04-09 20:17:48 +0200

    rpi: Use the cheapest vm B1ls, 36 SEK/month (from B2s, 287 SEK/month)
    Estimated monthly cost is $3, compared to the previous $25.

--- raspberry-pi/create-vm.ps1
+++ raspberry-pi/create-vm.ps1
@@ -169,7 +169,7 @@ $vmName = "VM"
 $vmConfig = `
   New-AzVMConfig `
     -VMName $vmName `
-    -VMSize "Standard_B2s" | `
+    -VMSize "Standard_B1ls" | `
   Set-AzVMOperatingSystem `
     -Linux `
     -ComputerName $vmName `
