c1955776aee1f652494e88b2cc9cc490a26b9d62
John-Philip Johansson <seriema@gmail.com>
2020-04-09 22:02:26 +0200

    rpi: Raise vm cost to B1s, 72 SEK/month
    B1ls was too cheap. Failed during installation of prerequisites for
    mounting Azure File Share:
    > "Too long with no output (exceeded 10m0s): context deadline exceeded"
    
    B1s is twice as expensive as B1ls: $6/month.

--- raspberry-pi/create-vm.ps1
+++ raspberry-pi/create-vm.ps1
@@ -169,7 +169,7 @@ $vmName = "VM"
 $vmConfig = `
   New-AzVMConfig `
     -VMName $vmName `
-    -VMSize "Standard_B1ls" | `
+    -VMSize "Standard_B1s" | `
   Set-AzVMOperatingSystem `
     -Linux `
     -ComputerName $vmName `
