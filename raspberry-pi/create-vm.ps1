21a3131366f927dd14ed64331a1a14783486f45c
John-Philip Johansson <seriema@gmail.com>
2020-04-10 07:03:38 +0200

    rpi: Raise vm cost to A0, 114 SEK/month
    Skyscraper install would silently fail due to too few threads. It got
    fixed but I raised the VM size anyway as it'd probably fail on
    something else due to being to slow.

--- raspberry-pi/create-vm.ps1
+++ raspberry-pi/create-vm.ps1
@@ -169,7 +169,7 @@ $vmName = "VM"
 $vmConfig = `
   New-AzVMConfig `
     -VMName $vmName `
-    -VMSize "Standard_B1s" | `
+    -VMSize "Standard_A0" | `
   Set-AzVMOperatingSystem `
     -Linux `
     -ComputerName $vmName `
