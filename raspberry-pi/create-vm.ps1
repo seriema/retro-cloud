1bfe144769e2d525e4c3647a652bd970a6d658dd
John-Philip Johansson <seriema@gmail.com>
2020-04-10 13:56:42 +0200

    rpi: Raise vm cost to D1, 465 SEK/month
    B1s is enough. It's 72 SEK/month and CI step only takes 1min longer
    (5->6m) than B2s that costs 287 SEK/month.
    
    But every CI build only costs around 0.20 SEK so it's worth exploring
    how fast the builds can get by paying for a faster VM. I could then use
    a cheap VM for users so their costs are as low as possible (they can
    always scale up), but have builds that are as fast as I want to pay for.

--- raspberry-pi/create-vm.ps1
+++ raspberry-pi/create-vm.ps1
@@ -169,7 +169,7 @@ $vmName = "VM"
 $vmConfig = `
   New-AzVMConfig `
     -VMName $vmName `
-    -VMSize "Standard_DS1_v2" | `
+    -VMSize "Standard_D1" | `
   Set-AzVMOperatingSystem `
     -Linux `
     -ComputerName $vmName `
