8870a2a34216102335a6acf077ae7ad34fb52b1a
John-Philip Johansson <seriema@gmail.com>
2020-04-10 14:00:29 +0200

    rpi: Raise vm cost to F2s_v2, 612 SEK/month
    B1s is enough. It's 72 SEK/month and CI step only takes 1min longer
    (5->6m) than B2s that costs 287 SEK/month.
    
    But every CI build only costs around 0.20 SEK so it's worth exploring
    how fast the builds can get by paying for a faster VM. I could then use
    a cheap VM for users so their costs are as low as possible (they can
    always scale up), but have builds that are as fast as I want to pay for.

--- raspberry-pi/create-vm.ps1
+++ raspberry-pi/create-vm.ps1
@@ -169,7 +169,7 @@ $vmName = "VM"
 $vmConfig = `
   New-AzVMConfig `
     -VMName $vmName `
-    -VMSize "Standard_B2ms" | `
+    -VMSize "Standard_F2s_v2" | `
   Set-AzVMOperatingSystem `
     -Linux `
     -ComputerName $vmName `
