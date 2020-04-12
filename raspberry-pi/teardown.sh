aea708fca40612710a9b98c0f31d4e52f3ac48a5
John-Philip Johansson <seriema@gmail.com>
2020-04-12 15:00:57 +0200

    rpi: Allow for Az teardown to work when create-vm fails
    Store the resource group name in $HOME/.retro-cloud.env in the beginning
    of create-vm.ps1 instead of the end, and source it from teardown.sh in
    case the variables aren't loaded into the environment variables.
    
    Some background:
    
    Sometimes the `create-vm.ps1` script fails:
    * Over budget, cannot create more resources
    * Too many public IP's assigned in the same region (max 10)
    * Azure under too much pressure and cannot allocate a VM in the
      requested region
    * RPi or Docker image isn't as expected (SSH keys being the most common)
    
    The resource group name was only stored at the end of create-vm.ps1,
    because we do not want to touch the users .bashrc unless we're sure it's
    worth it, so teardown-az.ps1 would fail:
    * in CI, leaving the resources from the failed setup to linger until I
      manually remove them
    * on RPi or Docker, leaving the user to search for the resource group
      name in the terminal output to give teardown (or use the Az Portal)

--- raspberry-pi/teardown.sh
+++ raspberry-pi/teardown.sh
@@ -3,6 +3,10 @@
 # Abort on error, and error if variable is unset
 set -eu
 
+# Disable lint: the .retro-cloud.env file is created by create-vm.ps1 so it can't be included for shellcheck (https://github.com/koalaman/shellcheck/wiki/SC1091)
+# shellcheck disable=SC1090
+source "$HOME/.retro-cloud.env"
+
 echo "TEARDOWN: Run PowerShell scripts to remove the Azure resources"
 pwsh -executionpolicy bypass -File ".\teardown-az.ps1"
 
