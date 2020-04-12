5bd4d5eaa04bf72f2595f65908a95d4aac6c1678
John-Philip Johansson <seriema@gmail.com>
2020-04-12 08:32:57 +0200

    ci: Use the "commit" workflow to trigger CircleCI builds
    It's a preview feature but it works. Keeping the builds grouped by
    workflow makes it much easier to get an overview of progress in CircleCI's
    web UI, and more likely to queue builds in a way that don't run more
    scriptValidation jobs than Azure allows for public IP's (max 10/region).
    
    Example:
    https://circleci.com/docs/api/#trigger-a-new-build-by-project-preview

--- .circleci/build-all-commits.sh
+++ .circleci/build-all-commits.sh
@@ -9,18 +9,13 @@ logfile=$(createLog circleci build)
 branch=$(git rev-parse --abbrev-ref HEAD)
 # Skip the latest commit as it will be automatically built by the branch being pushed.
 mapfile -t commits < <(git log "develop...HEAD~1" --format="%h")
-jobs=( bashValidation imageValidation scriptValidation )
 
 for commit in "${commits[@]}"
 do
-    for job in "${jobs[@]}"
-    do
-        # https://circleci.com/docs/2.0/api-job-trigger/#overview
-        curl -u "${CIRCLECI_API_USER_TOKEN}:" \
-            --data "build_parameters[CIRCLE_JOB]=$job" \
-            --data "build_parameters[CIRCLE_COMPARE_URL]=https://github.com/seriema/retro-cloud/compare/develop...$branch" \
-            --data "revision=$commit" \
-            "https://circleci.com/api/v1.1/project/github/seriema/retro-cloud/tree/${branch}" \
-        | tee -a "$logfile"
-    done
+    curl \
+        -u "${CIRCLECI_API_USER_TOKEN}:" \
+        --data "revision=$commit" \
+        --data "branch=$branch" \
+        "https://circleci.com/api/v1.1/project/github/seriema/retro-cloud/build" \
+    | tee -a "$logfile"
 done
