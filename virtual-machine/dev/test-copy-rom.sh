a73055f636e3928779d0958fe65e9fd37712a97c
John-Philip Johansson <seriema@gmail.com>
2020-04-18 18:45:47 +0200

    vm: Make copy rom script more chatty
    Changing the Ci scripts was harder to compare when the scripts aren't
    fully outputed beforehand. Having the actions more verbose helps.

--- virtual-machine/dev/test-copy-rom.sh
+++ virtual-machine/dev/test-copy-rom.sh
@@ -3,9 +3,9 @@
 # Abort on error, and error if variable is unset
 set -eu
 
-# Copy a tiny (82K) freeware game from the makers website
+echo 'Copy a tiny (82K) freeware game from the makers website'
 curl -fOL http://www.elitehomepage.org/archive/a/b7120500.zip
 
-# Move it and give it a better name otherwise the scrapers won't find it
+echo 'Move it and give it a better name so the scrapers find it'
 sudo mkdir -p "$RETROCLOUD_VM_SHARE/RetroPie/roms/nes"
-sudo mv "b7120500.zip" "$_/elite.zip"
+sudo mv -v "b7120500.zip" "$_/elite.zip"
