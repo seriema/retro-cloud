c43ef7cf5a4d3c2514940437e3bb69b239b50484
John-Philip Johansson <seriema@gmail.com>
2020-04-18 10:07:33 +0200

    dev: Prefix lint log messages with "Lint:"

--- lint.sh
+++ lint.sh
@@ -3,11 +3,11 @@
 # Abort on error, and error if variable is unset
 set -eu
 
-echo 'Check for Bash scripts without execute permission.'
+echo 'LINT: Check for Bash scripts without execute permission.'
 find . -type f -name '*.sh' -not -executable
 
-echo 'Lint with shellcheck.'
+echo 'LINT: Lint with shellcheck.'
 # Runs shellcheck as a docker app so you don't need it installed.
 find . -type f -name '*.sh' -print0 | xargs -0 docker run --rm --volume "$PWD:/mnt" --workdir //mnt koalaman/shellcheck:stable --external-sources
 
-echo 'Done.'
+echo 'LINT: Done.'
