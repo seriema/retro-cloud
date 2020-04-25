1aa885a9b669b3d6f94a3fef37c9c3d3204d57b8
John-Philip Johansson <seriema@gmail.com>
2020-04-25 12:18:53 +0200

    dev: Use docker compose in start script
    Simplifies it a bit a lot.

--- start.sh
+++ start.sh
@@ -12,28 +12,7 @@ docker pull "$tag"
 
 # Create a machine specific container
 if [[ $(getArch) == "arm32v7" ]]; then # Raspberry Pi
-    containerInstance=$(docker container create \
-        --env PULSE_SERVER=unix:/run/user/1000/pulse/native \
-        --group-add input \
-        --group-add video \
-        --interactive \
-        --privileged \
-        --rm \
-        --tty \
-        `# Mount various volumes that enable devices (e.g. joypad, video, etc) to pass through to the container.` \
-        --volume /dev/fb0:/dev/fb0 \
-        --volume /dev/input:/dev/input \
-        --volume /dev/shm:/dev/shm \
-        --volume /dev/snd:/dev/snd \
-        --volume /dev/usb:/dev/usb \
-        --volume /dev/vchiq:/dev/vchiq \
-        --volume /dev/vcio:/dev/vcio \
-        --volume /dev/vcsm:/dev/vcsm \
-        --volume /opt/vc:/opt/vc \
-        --volume /run/user/1000:/run/user/1000 \
-        --volume /var/run/dbus/:/var/run/dbus/ \
-        "$tag" \
-    )
+    containerInstance=$(docker-compose -f docker-compose.yml -f docker-compose.arm32v7.yml run -d --rm rpi bash)
 
     # We need the RPi configured controllers to connect and they need to be writable because EmulationStation always writes to them, but not risk breaking the RPi configs
     docker cp --follow-link /opt/retropie/configs/all/retroarch/. "${containerInstance}:/opt/retropie/configs/all/retroarch"
@@ -41,18 +20,11 @@ if [[ $(getArch) == "arm32v7" ]]; then # Raspberry Pi
     docker cp /opt/retropie/configs/all/emulationstation/es_input.cfg "${containerInstance}:/opt/retropie/configs/all/emulationstation/es_input.cfg"
     docker cp /opt/retropie/configs/all/emulationstation/es_temporaryinput.cfg "${containerInstance}:/opt/retropie/configs/all/emulationstation/es_temporaryinput.cfg"
 
+    # Run the prepared container
+    docker container start --attach --interactive "$containerInstance"
+
 else # Windows
-    containerInstance=$(docker container create \
-        --cap-add SYS_ADMIN \
-        --device /dev/fuse \
-        --interactive \
-        --rm \
-        --tty \
-        "$tag" \
-    )
+    docker-compose run --rm rpi bash
 fi
 
 # Do not copy the host's SSH keys, if this container is to mimic a user's RetroPie
-
-# Run the prepared container
-docker container start --attach --interactive "$containerInstance"
