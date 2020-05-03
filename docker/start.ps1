a3f88f0bfe90c2a305fe26cb198ab2798aeb7323
John-Philip Johansson <seriema@gmail.com>
2020-05-03 18:15:57 +0200

    dev: Improve Windows Docker start dev PS script
    Match it functionality wise with Bash so it's actually useful on Windows.

--- docker/start.ps1
+++ docker/start.ps1
@@ -5,7 +5,8 @@ param (
 # Abort on error
 $ErrorActionPreference = "Stop"
 
-docker run `
+"Create the container"
+$containerInstance = docker container create `
     --cap-add SYS_ADMIN `
     --device /dev/fuse `
     --env-file .env `
@@ -13,6 +14,22 @@ docker run `
     --rm `
     --tty `
     --volume powershell-install:/opt/microsoft/powershell `
+    --volume powershell-modules-cache:/home/pi/.cache/powershell `
+    --volume powershell-modules-config:/home/pi/.config/NuGet `
+    --volume powershell-azure-context:/home/pi/.Azure `
     --volume "$($PWD.Path):/home/pi/retro-cloud-source" `
     --workdir /home/pi/retro-cloud-source `
     "rc:$branch"
+
+"Copy SSH"
+# We want writable .ssh/known_hosts but not risk that our own .ssh/ directory gets broken
+docker cp "$HOME/.ssh" "${containerInstance}:/home/pi/.ssh"
+
+"Start the prepared container"
+docker container start "$containerInstance"
+
+"Symlink the PowerShell binary as 'pwsh', like install-ps.sh does"
+docker exec -d "$containerInstance" sudo ln --symbolic "/opt/microsoft/powershell/7/pwsh" "/usr/bin/pwsh"
+
+"Attach to the container"
+docker container attach "$containerInstance"
