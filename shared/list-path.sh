899401478cc32f9d5af905dcef6d713ddd04e3f5
John-Philip Johansson <seriema@gmail.com>
2020-05-17 17:12:55 +0200

    all: Print without sudo
    Rights elevation inside the script was failing when printing /mnt when
    successfully mounted to VM. Which is not detectable on CircleCI.

--- shared/list-path.sh
+++ shared/list-path.sh
@@ -5,4 +5,4 @@ path=${1:-"$HOME"}
 # Abort on error, error if variable is unset, and error if any pipeline element fails
 set -euo pipefail
 
-sudo find "$path" | sed -e "s/[^-][^\/]*\// |/g" -e "s/|\([^ ]\)/|-\1/"
+find "$path" | sed -e "s/[^-][^\/]*\// |/g" -e "s/|\([^ ]\)/|-\1/"
